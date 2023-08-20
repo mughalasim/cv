3562272c1b6a62b56a216a5943a24bdb2b73f828
Asim Mughal <asimkenya@gmail.com>
2023-08-20 16:52:38 +0100

    Added SettingsScreen, ButtonWidget and cleaned up previews and resolved minifying issues

--- domain/src/main/java/cv/domain/entities/SettingsEntity.kt
+++ domain/src/main/java/cv/domain/entities/SettingsEntity.kt
@@ -1,8 +1,14 @@
 package cv.domain.entities
 
+import androidx.annotation.Keep
+
+@Keep
+@Suppress("unused")
 data class SettingsEntity (
     val expandListOnStartUp: Boolean,
-)
+){
+    constructor() : this(false)
+}
 
 fun getFakeSettingsEntity() =
     SettingsEntity(
